55a92023c5019efdfe96609b799d093c8742da85
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-09-20 16:16:09 -0500

    pet adoption

---
+++ routes/api/pet.js
@@ -0,0 +1,112 @@
+const debug = require('debug')('app:routes:api:pet');
+const debugError = require('debug')('app:error');
+const express = require('express');
+const { nanoid } = require('nanoid');
+
+module.exports = {
+  add: (x, y) => x + y,
+  sub: (x, y) => x - y,
+}
+
+const petsArray = [
+  {_id: "1", name: "Waltuh"},
+  {_id: "2", name: "AUGHHHH"},
+  {_id: "3", name: "Wait, Wait, Wait..."}
+];
+
+// create a router
+const router = express.Router();
+
+//define routes
+router.get('/list', (req, res, next) => {
+  res.json(petsArray);
+});
+router.get('/:petId', (req, res, next) => {
+  const petId = req.params.petId;
+
+  // array lookup
+  // const pet = petsArray[petId];
+  // res.json(pet);
+
+  // linear search
+  // let pet;
+  // for (const p of petsArray) {
+  //   if(p.name == petId) {
+  //     pet = p;
+  //     break;
+  //   }
+  // }
+  // res.json(pet);
+
+  //using find
+  const pet = petsArray.find(x => x._id == petId);
+  if(!pet) {
+    res.status(404).json({ error: "Pet not found" });
+  } else{
+    res.json(pet);
+  }
+  
+});
+router.put('/new', (req, res, next) => {
+  const petId = nanoid();
+  const { species, name, gender } = req.body;
+  const age = parseInt(req.body.age)
+  const pet = {
+    _id: petId,
+    species,
+    name,
+    age,
+    gender,
+    createdDate: new Date(),
+  };
+
+  if(!species) {
+    res.status(400).json({ error: 'Species required.' });
+  }else if (!name) {
+    res.status(400).json({ error: 'Name required.' });
+  }else if (!age) {
+    res.status(400).json({ error: 'Age required.' });
+  }else if (!gender) {
+    res.status(400).json({ error: 'Gender required.' });
+  }else {
+    petsArray.push(pet);
+    res.json(pet);
+  }
+});
+router.put('/:petId', (req, res, next) => {
+  const petId = req.params.petId;
+  const { species, name, age, gender } = req.body;
+
+  const pet = petsArray.find(x => x._id == petId)
+  if(!pet) {
+    res.status(404).json({ error: 'Pet not found.' })
+  }else {
+    if(species != undefined) {
+      pet.species = age;
+    }
+    if(name !=undefined) {
+      pet.name = name;
+    }
+    if(age != undefined) {
+      pet.age = parseInt(age);
+    }
+    if(gender != undefined) {
+      pet.gender = gender;
+    }
+    pet.lastUpdated = new Date();
+    res.json(pet);
+  }
+});
+router.delete('/:petId', (req, res, next) => {
+  const petId = req.params.petId;
+  const index = petsArray.findIndex(x => x._id == petId);
+  if(index < 0) {
+    res.status(404).json({ error: 'Pet not found.' });
+  } else {
+    petsArray.splice(index, 1);
+    res.json({ message: 'Pet Deleted.' })
+  }
+});
+
+// export the router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const debug = require('debug')('app:server');
 const debugError = require('debug')('app:error');
 const express = require('express');
-//const nanoid = require('nanoid');
 
 // construct express app
 const app = express();
@@ -9,42 +8,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const petsArray = [
-  {_id: "1", name: "Fido"},
-  {_id: "2", name: "Watson"},
-  {_id: "3", name: "Loki"}
-];
-
 // define routes
-app.get('/api/pet/list', (req, res, next) => {
-  res.json(petsArray);
+app.use('/api/pet', require('./routes/api/pet'));
+
+// handle errors
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Page not found."});
 });
-app.get('/api/pet/:petId', (req, res, next) => {
-  const petId = req.params.petId;
-
-  // array lookup
-  // const pet = petsArray[petId];
-  // res.json(pet);
-
-  // linear search
-  // let pet;
-  // for (const p of petsArray) {
-  //   if(p.name == petId) {
-  //     pet = p;
-  //     break;
-  //   }
-  // }
-  // res.json(pet);
-
-  //using find
-  const pet = petsArray.find(x => x._id == petId);
-  if(!pet) {
-    res.status(404).json({ error: "Pet not found" })
-  } else{
-    res.json(pet);
-  }
-  
-})
+app.use((err, req, res, next) => {
+  debugError(err);
+  res.status(500).json({ error: err.message });
+});
 
 // start listening for requests
 const port = process.env.PORT || 5000;
